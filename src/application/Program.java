ee316a010e7bec5c1259c18c0a40dfc23f9967fa
higorasilverio <higorasilverio@outlook.com>
2020-04-23 08:17:05 -0300

    Changing the try/catch logic to improve coding - Final

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -18,9 +20,12 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<Product>();
 
 		System.out.print("Enter the .csv file path: ");
 		String inStringPath = input.next();
+		
 		File inFilePath = new File(inStringPath);
 		String outStringPath = inFilePath.getParent();
 		new File(outStringPath + "\\out").mkdir();
@@ -28,40 +33,37 @@ public class Program {
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(inStringPath))) {
 			String lineInRead = br.readLine();
-			String lineParts[] = new String[3];
-			boolean fileCriationControl = false;				
+				
 			while (lineInRead != null) {
-				lineParts = lineInRead.split(",");
+				String lineParts[] = lineInRead.split(",");
 				String name = lineParts[0];
 				double price = Double.parseDouble(lineParts[1]);
 				int quantity = Integer.parseInt(lineParts[2]);
-				Product product = new Product(name, price, quantity);
-				if (fileCriationControl == false) {
-					try (BufferedWriter bw = new BufferedWriter(new FileWriter(outStringPath + "\\out\\summary.csv"))) {
-						bw.write(product.getName() + "," + product.totalValue());
-						bw.newLine();
-					} catch (IOException e) {
-						System.out.println("Error creating file '...\\out\\summary.csv': " + e.getMessage());
-					} finally {
-						fileCriationControl = true;
-					}
-				} else {
-					try (BufferedWriter bw = new BufferedWriter(new FileWriter(outStringPath + "\\out\\summary.csv", true))) {
-						bw.write(product.getName() + "," + product.totalValue());
-						bw.newLine();
-					} catch (IOException e) {
-						System.out.println("Error updating file '...\\out\\summary.csv': " + e.getMessage());
-					}
-				}
+				
+				list.add(new Product(name, price, quantity));
+				
 				lineInRead = br.readLine();
 			}
+			
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(outStringPath + "\\out\\summary.csv"))){
+				for (Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.totalValue()));
+					bw.newLine();
+				}
+				System.out.println("File '...\\out\\summary.csv' created successfully!");
+			}
+			catch (IOException e) {
+				System.out.println("Error creating file '...\\out\\summary.csv': " + e.getMessage());
+			}
+				
 		} 
 		catch (FileNotFoundException e) {
-			System.out.println("Error: " + e.getMessage());
+			System.out.println("Error searching for file: " + e.getMessage());
 		} 
 		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+			System.out.println("Error reading file: " + e.getMessage());
 		}
+		System.out.println("Data for product total value available in: " + outStringPath + "\\out\\summary.csv");
 		input.close();
 	}
 
